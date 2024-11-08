0dceba36bec7dd8e39fb58e2d4ac1df31c81045a
muccijoao <163357832+muccijoao@users.noreply.github.com>
2024-11-07 21:18:30 -0300

    Update main.c
    adicionei o selection sort e o insertion sort

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 
+void selectionSort(int arr[], int n);
+void insertionSort(int arr[], int n);
 void mergeSort(int *vetor, int tamanho);
 void bubbleSort(int *vetor, int tamanho);
 
@@ -91,5 +93,34 @@ void bubbleSort(int *vetor, int tamanho) {
                 vetor[proximo + 1] = auxiliar;
             }
         }
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i; 
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
     }
 }
